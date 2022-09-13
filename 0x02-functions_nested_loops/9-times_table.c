4c46b4a775db07a2ad6ab268ef1cfbac9cce6557
bekalu <bekisisay2020@gmail.com>
2022-09-13 16:24:14 -0700

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,16 +14,13 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-			
 			prod = num * mult;
-			
 			if (prod <= 9)
 				_putchar(' ');
-			else 
+			else
 				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0'); 
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
 }
-			
