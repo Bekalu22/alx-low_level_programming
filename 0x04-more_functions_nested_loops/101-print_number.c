f3eab8100cb6e2747c89a59938cfc68addd3bde3
bekalu <bekisisay2020@gmail.com>
2022-09-16 13:47:42 -0700

    all taks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,16 @@
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		x = -n;
+		putchar('-');
+		num = -num;
 	}
-	if (x / 10)
-		print_number(x / 10);
-	putchar((x % 10) + '0');
+	if (num > 0)
+	{
+		print_number(num / 10);
+	}
+	putchar(num % 10 + '0');
 }
