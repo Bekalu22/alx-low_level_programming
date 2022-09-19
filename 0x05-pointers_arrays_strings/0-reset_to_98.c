22869f714588e07451b4be0d29fb777a95181d0a
bekalu <bekisisay2020@gmail.com>
2022-09-19 12:58:58 -0700

    swaps the values of two integers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
-<<<<<<< HEAD
-
 /**
- * *reset_to_98 - resets the value of given integer to 98
+ *reset_to_98 - resets the value of given integer to 98
  *
  *@n: parameter
  *
@@ -11,17 +9,5 @@
  */
 void reset_to_98(int *n)
 {
-	        *n = 98;
-=======
-/**
-*reset_to_98 - resets the value of given integer to 98
-*
-*@n: parameter
-*
-*Return: nothing
-*/
-void reset_to_98(int *n)
-{
 	*n = 98;
->>>>>>> 266b5e3b1d5ab01cf2b7b38d9cff981bb3ce5466
 }
