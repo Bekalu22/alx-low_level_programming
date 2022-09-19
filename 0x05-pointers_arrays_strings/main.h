8b539bccf43511f23a85ac797cad12e3fc9d1d94
bekalu <bekisisay2020@gmail.com>
2022-09-19 13:05:32 -0700

    swaps the values of two integers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,15 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
-=======
 /**
 *void prototypes()
 *
 *int prototypes()
 */
 
->>>>>>> 266b5e3b1d5ab01cf2b7b38d9cff981bb3ce5466
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -22,9 +19,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-<<<<<<< HEAD
-
 #endif   /* MAIN_H */
-=======
-#endif  /* MAIN_H */
->>>>>>> 266b5e3b1d5ab01cf2b7b38d9cff981bb3ce5466
