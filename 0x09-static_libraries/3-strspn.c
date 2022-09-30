7668671899c4abc77c798ee998cde44d555e7ec9
bekalu <bekisisay2020@gmail.com>
2022-09-30 07:59:29 -0700

    static library libmy.a containing all the functions listed

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - prints the consecutive characters of s1 that are in s2.
+ *
+ * @s: source string
+ *
+ * @accept: searching string
+ *
+ * Return: new string.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0, b, t = 0;
+
+	while (accept[a])
+	{
+		b = 0;
+
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+
+			b++;
+		}
+		a++;
+	}
+
+	return (t);
+}
