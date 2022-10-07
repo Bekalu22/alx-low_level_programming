7b403c64e9428f3500fc4520596fd01ad297dc6e
bekalu <bekisisay2020@gmail.com>
2022-10-07 13:34:49 -0700

    multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -136,10 +136,10 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 * @next_len: the length of next_prod.
 */
 void add_nums(char *final_prod, char *next_prod, int next_len)
-	{
+{
 		int num, tens = 0;
 
-		while (*(final_rod + 1))
+		while (*(final_prod + 1))
 			final_prod++;
 
 		while (*(next_prod + 1))
@@ -155,20 +155,17 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 			next_prod--;
 			next_len--;
 		}
-	}
-	for (; nextlen >= 0 && *next_prod != 'x'; next_len--)
-	{
-		num = (*next_prod - '0');
-		num += tens;
-		*final_prod = (num % 10) + '0';
-		tens = num / 10;
-
-		final_prod--;
-		next_prod--;
-	}
-
-	if (tens)
-		*final_prod = (tens % 10) + '0';
+		for (; next_len >= 0 && *next_prod != 'x'; next_len--)
+		{
+			num = (*next_prod - '0');
+			num += tens;
+			*final_prod = (num % 10) + '0';
+			tens = num / 10;
+			final_prod--;
+			next_prod--;
+		}
+		if (tens)
+			*final_prod = (tens % 10) + '0';
 }
 
 /**
